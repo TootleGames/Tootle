b7423a179e9e933399d01093e62ceea19ad10017
Duane Bradbury <dibely@hotmail.com>
2009-02-09 17:29:45 +0000

    Updated scheme load and unload work so I can test it on PC.

--- Code/TootleScene/TSchemeManager.cpp
+++ Code/TootleScene/TSchemeManager.cpp
@@ -9,6 +9,9 @@
 
 #include "TSchemeManager.h"
 
+#include <TootleAsset/TAsset.h>
+#include <TootleCore/TEventChannel.h>
+
 namespace TLScheme
 {
 	TPtr<TSchemeManager> g_pSchemeManager = NULL;
@@ -22,28 +25,343 @@ SyncBool TSchemeManager::Initialise()
 	return SyncTrue; 
 }
 
+void TSchemeManager::OnEventChannelAdded(TRefRef refPublisherID, TRefRef refChannelID)
+{
+	if(refPublisherID == "CORE")
+	{
+		// Subscribe to the update messages
+		if(refChannelID == TLCore::UpdateRef)
+			TLMessaging::g_pEventChannelManager->SubscribeTo(this, refPublisherID, refChannelID); 
+	}
+	
+	// Super event channel routine
+	TManager::OnEventChannelAdded(refPublisherID, refChannelID);
+}
+
+
 SyncBool TSchemeManager::Shutdown()
 { 
+	// While we still have schemes loaded request them to be unloaded
+	if(m_Schemes.GetSize())
+	{
+		//NOTE: We may not need to do this as we can assume the scenegraph will shutdown and remove all nodes
+		UnloadAllSchemes();
+		
+		// Wait for all schemes to have been unloaded.
+		return SyncWait;
+	}	
+	
+	// Remove all request objects
+	for(u32 uIndex = 0; uIndex < m_SchemeUpdateRequests.GetSize(); uIndex++)
+	{
+		m_SchemeUpdateRequests.ElementAt(uIndex) = NULL;
+	}
+
+	// Empty request list
+	m_SchemeUpdateRequests.Empty();
+	
+	// Empty the list of remove requests
+	m_SchemeUpdateRemove.Empty();
+	
 	return SyncTrue; 
 }
 
-SyncBool TSchemeManager::Update(float /*fTimeStep*/)		
+SyncBool TSchemeManager::Update(float fTimeStep)		
 {
-	return SyncTrue;
+	u32 uNumberOfRequests = m_SchemeUpdateRequests.GetSize();
+	
+	if(uNumberOfRequests > 0)
+	{
+		// Update all of the scheme update requests
+		for(u32 uIndex = 0; uIndex < uNumberOfRequests; uIndex++)
+		{
+			TPtr<TSchemeUpdateRequest> pRequest = m_SchemeUpdateRequests.ElementAt(uIndex);			
+
+			pRequest->Update();
+		}
+	}
+
+	// Now go through the list of update requests that need removing if any
+	uNumberOfRequests = m_SchemeUpdateRemove.GetSize();
+
+	if(uNumberOfRequests)
+	{
+		for(u32 uIndex = 0; uIndex < uNumberOfRequests; uIndex++)
+		{
+			TRefRef SchemeRef = m_SchemeUpdateRemove.ElementAt(uIndex);
+			
+			s32 sIndex = m_SchemeUpdateRequests.FindIndex(SchemeRef);
+			
+			if(sIndex != -1)
+			{
+				// remove from the list
+				m_SchemeUpdateRequests.RemoveAt(sIndex);				
+			}
+			else
+			{
+				TLDebug_Break("Failed to find scheme update request object for removal");
+			}
+		}
+
+		// Empty the list of remove requests
+		m_SchemeUpdateRemove.Empty();
+	}
+	
+	return TManager::Update(fTimeStep);
 }
 
 
-Bool TSchemeManager::IsSchemeLoaded(TRefRef SchemeRef)
+Bool TSchemeManager::IsSchemeInState(TRefRef SchemeRef, TSchemeState SchemeState)
 {
+	if(SchemeState == Loaded)
+	{
+		return (m_Schemes.Find(SchemeRef) != NULL);
+	}
+	else if(SchemeState == UnLoaded)
+	{
+		return (m_Schemes.Find(SchemeRef) == NULL);
+	}
+	else
+	{
+		TPtr<TSchemeUpdateRequest> pRequest = m_SchemeUpdateRequests.FindPtr(SchemeRef);
+
+		// Any request to load or unload? If not we are not loading or unloading the scheme
+		if(!pRequest)
+			return FALSE;
+
+		// If the request is a load then return TRUE is the state check was Loading
+		if((pRequest->GetUpdateType() == Load) && (SchemeState == Loading))
+			return TRUE;
+		else if(SchemeState == UnLoading)
+			return TRUE;
+			
+		return FALSE;
+	}
+}
+
+// Main request handling routine
+Bool TSchemeManager::RequestUpdateScheme(TRefRef SchemeRef, TRefRef SchemeAssetRef, TSchemeUpdateType UpdateType)
+{
+	// Check to see if the scheme specified is alreayd in the state required
+	if(UpdateType == Load)
+	{
+		// Laoded already?
+		if(IsSchemeLoaded(SchemeRef))
+			return TRUE;
+		
+		// Loading requested already?
+		if(IsSchemeLoading(SchemeRef))
+			return TRUE;
+		
+		// Unload requested?
+		// Scheme loading?
+		if(IsSchemeUnloading(SchemeRef))
+		{
+			// Cancel the unload and start the load
+			TLDebug_Break("TODO");
+			return FALSE;
+		}
+		
+	}	
+	else
+	{
+		// Unloaded already?
+		if(IsSchemeUnLoaded(SchemeRef))
+			return TRUE;
+
+		// Unloading already?
+		if(IsSchemeUnloading(SchemeRef))
+			return TRUE;
+		
+		// Scheme loading?
+		if(IsSchemeLoading(SchemeRef))
+		{
+			// Cancel the load and start the unload
+			TLDebug_Break("TODO");
+			return FALSE;
+		}
+		
+	}
+	
+	
+	// Begin the load/unload of the scheme
+	TPtr<TSchemeUpdateRequest> pRequest = new TSchemeUpdateRequest(SchemeRef, SchemeAssetRef, UpdateType);
+	
+	if(pRequest)
+	{
+		if(m_SchemeUpdateRequests.Add(pRequest) != -1)
+		{
+			// Subscribe to the update request
+			return SubscribeTo(pRequest.GetObject());
+		}
+	}
+	
+
+	
 	return FALSE;
 }
 
-Bool TSchemeManager::RequestLoadScheme(TRefRef SchemeRef)
+void TSchemeManager::UnloadAllSchemes()
 {
-	return FALSE;
+	for(u32 uIndex = 0; uIndex < m_Schemes.GetSize(); uIndex++)
+	{
+		TRefRef SchemeRef = m_Schemes.ElementAt(uIndex);
+		RequestUpdateScheme(SchemeRef, "", UnLoad);
+	}
 }
 
-Bool TSchemeManager::RequestUnloadScheme(TRefRef SchemeRef)
+void TSchemeManager::ProcessMessage(TPtr<TLMessaging::TMessage>& pMessage)
 {
-	return FALSE;
+	TRef MessageRef = pMessage->GetMessageRef();
+
+	// Handle request update messages
+	if(MessageRef == "LOAD")
+	{
+		TRef SchemeRef;
+		
+		if(pMessage->ImportData("SCHEME", SchemeRef))
+		{
+			// If complete then add the scheme to our list of instanced schemes
+			// and remove from the schemeupdate list
+			m_Schemes.Add(SchemeRef);
+			
+			m_SchemeUpdateRemove.Add(SchemeRef);
+				
+		}
+		
+		return;
+	}
+	else if(MessageRef == "UNLOAD")
+	{
+		// If complete then remove the scheme from our list of instanced schemes
+		// and remove from the schemeupdate list
+		TRef SchemeRef;
+		
+		if(pMessage->ImportData("SCHEME", SchemeRef))
+		{
+			// If complete then add the scheme to our list of instanced schemes
+			// and remove from the schemeupdate list			
+			m_Schemes.Remove(SchemeRef);
+			
+			m_SchemeUpdateRemove.Add(SchemeRef);
+		}
+		
+		return;
+	}
+	
+	TManager::ProcessMessage(pMessage);
 }
+
+
+
+
+// Scheme update request class
+TSchemeManager::TSchemeUpdateRequest::TSchemeUpdateRequest(TRefRef SchemeRef, TRefRef SchemeAssetRef, TSchemeUpdateType UpdateType) :
+	m_SchemeRef(SchemeRef),
+	m_SchemeAssetRef(SchemeAssetRef),
+	m_Type(UpdateType)
+{
+	AddMode<TSchemeManager::TSchemeUpdateRequest::TSchemeState_Init>("Init");
+	AddMode<TSchemeManager::TSchemeUpdateRequest::TSchemeState_Loading>("Loading");
+	AddMode<TSchemeManager::TSchemeUpdateRequest::TSchemeState_UnLoading>("UnLoading");
+	AddMode<TSchemeManager::TSchemeUpdateRequest::TSchemeState_Finished>("Finished");
+	AddMode<TSchemeManager::TSchemeUpdateRequest::TSchemeState_Cancel>("Cancel");
+	
+	//SetMode("Init");
+}
+
+
+TRef TSchemeManager::TSchemeUpdateRequest::TSchemeState_Init::Update()
+{	
+	TPtr<TSchemeManager::TSchemeUpdateRequest> pRequest = GetStateMachine<TSchemeManager::TSchemeUpdateRequest>();
+
+	if(pRequest->GetUpdateType() == Load)
+	{
+		// Begin load		
+		TPtr<TLAsset::TAsset>& pAsset = TLAsset::GetAsset(pRequest->GetSchemeAssetRef(), TRUE);
+
+		// Is the asset loaded?
+		if(!pAsset)
+		{
+			// Request the asset to be loaded
+			TLAsset::LoadAsset(pRequest->GetSchemeAssetRef());
+			
+			// Wait for asset to load
+			return TRef();
+		}
+
+		// Asset is loaded
+		return "Loading";
+	}
+	else
+	{
+		// Begin unload
+		
+		return "UnLoading";
+	}
+}	
+
+
+TRef TSchemeManager::TSchemeUpdateRequest::TSchemeState_Loading::Update()
+{	
+	// Load the schemes required files
+	// Wait for files to load
+	// Instance the scheme
+	// Wait for scheme to be instanced
+	// Add scheme to scenegraph
+
+	// Broadcast message to say we are done loading
+	TPtr<TLMessaging::TMessage> pMessage = new TLMessaging::TMessage("LOAD");
+	
+	if(pMessage)
+	{
+		TPtr<TSchemeManager::TSchemeUpdateRequest> pRequest = GetStateMachine<TSchemeManager::TSchemeUpdateRequest>();
+		
+		if(pRequest)
+		{
+			pMessage->ExportData("SCHEME", pRequest->GetSchemeRef());
+			pRequest->PublishMessage(pMessage);
+		}
+	}
+
+	// All done	
+	return "Finished";	
+}	
+
+
+
+
+TRef TSchemeManager::TSchemeUpdateRequest::TSchemeState_UnLoading::Update()
+{	
+	// Find scheme node in scenegraph
+	// Shutdown the scheme node
+	// and remove from graph
+	
+	
+	// Broadcast message to say we are done loading
+	TPtr<TLMessaging::TMessage> pMessage = new TLMessaging::TMessage("LOAD");
+	
+	if(pMessage)
+	{
+		TPtr<TSchemeManager::TSchemeUpdateRequest> pRequest = GetStateMachine<TSchemeManager::TSchemeUpdateRequest>();
+		
+		if(pRequest)
+		{
+			pMessage->ExportData("SCHEME", pRequest->GetSchemeRef());
+			pRequest->PublishMessage(pMessage);
+		}
+	}
+	
+	// All done
+	return "Finished";	
+}	
+
+
+TRef TSchemeManager::TSchemeUpdateRequest::TSchemeState_Cancel::Update()
+{	
+	return TRef();	
+}	
+
+
+
+
--- Code/TootleScene/TSchemeManager.h
+++ Code/TootleScene/TSchemeManager.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <TootleCore/TManager.h>
+#include <TootleCore/TStateMachine.h>
 
 namespace TLScheme
 {
@@ -24,34 +25,116 @@ namespace TLScheme
 
 class TLScheme::TSchemeManager : public TManager
 {
-	typedef enum TUpdateType
+	enum TSchemeUpdateType
 	{
 		Load = 0,
 		UnLoad,
 	};
 	
+	enum TSchemeState
+	{
+		Loading = 0,
+		Loaded,
+		UnLoading,
+		UnLoaded,
+	};
+	
+	
 public:	
 	
-	Bool IsSchemeLoaded(TRefRef SchemeRef);
+	TSchemeManager(TRefRef ManagerRef) :
+		TManager( ManagerRef )
+	{
+	}
+
 	
-	Bool RequestLoadScheme(TRefRef SchemeRef); 
-	Bool RequestUnloadScheme(TRefRef SchemeRef);
+	FORCEINLINE Bool IsSchemeLoaded(TRefRef SchemeRef)			 { return IsSchemeInState(SchemeRef, Loaded); }
+	FORCEINLINE Bool IsSchemeLoading(TRefRef SchemeRef)			 { return IsSchemeInState(SchemeRef, Loading); }
+	FORCEINLINE Bool IsSchemeUnloading(TRefRef SchemeRef)		 { return IsSchemeInState(SchemeRef, UnLoading); }
+	FORCEINLINE Bool IsSchemeUnLoaded(TRefRef SchemeRef)		 { return IsSchemeInState(SchemeRef, UnLoaded); }
+	
+	FORCEINLINE Bool RequestLoadScheme(TRefRef SchemeRef, TRefRef SchemeAssetRef)		 { return RequestUpdateScheme(SchemeRef, SchemeAssetRef, Load); }
+	FORCEINLINE Bool RequestUnloadScheme(TRefRef SchemeRef, TRefRef SchemeAssetRef)		 { return RequestUpdateScheme(SchemeRef, SchemeAssetRef, UnLoad); }
 	
 protected:
 	virtual SyncBool Initialise(); 
 	virtual SyncBool Shutdown();
-	virtual SyncBool Update(float /*fTimeStep*/);		
+	virtual SyncBool Update(float /*fTimeStep*/);	
 	
-	Bool RequestUpdateScheme(TRefRef SchemeRef, TUpdateType UpdateRequestType);
+	virtual void	ProcessMessage(TPtr<TLMessaging::TMessage>& pMessage);
+
+	virtual void	OnEventChannelAdded(TRefRef refPublisherID, TRefRef refChannelID);
+
+	void		UnloadAllSchemes();
 	
-	// TUpdateRequest class
-	class TUpdateRequest
-	{
-		TUpdateType m_Type;
-	};
+	// Scheme state checking
+	Bool		IsSchemeInState(TRefRef SchemeRef, TSchemeState SchemeState);
+
+	// Scheme update request
+	Bool		RequestUpdateScheme(TRefRef SchemeRef, TRefRef SchemeAssetRef, TSchemeUpdateType UpdateRequestType);
+
+	// inernal TSchemeUpdateRequest class
+	class TSchemeUpdateRequest;
 
 private:
 
-	TArray<TRef>				m_Schemes;		// List of instanced schemes
-	TArray<TUpdateRequest>		m_UpdateRequests;
+	TArray<TRef>						m_Schemes;					// List of instanced schemes
+	TPtrArray<TSchemeUpdateRequest>		m_SchemeUpdateRequests;		// Scheme load/unload requests
+	TArray<TRef>						m_SchemeUpdateRemove;		// List of update requests to remove
+};
+
+// TSchemeUpdateRequest class
+class TLScheme::TSchemeManager::TSchemeUpdateRequest : public TStateMachine, public TPublisher
+{	
+public:
+	TSchemeUpdateRequest(TRefRef SchemeRef, TRefRef SchemeAssetRef, TSchemeUpdateType UpdateType);
+	
+	TRefRef				GetSchemeRef()			const	{ return m_SchemeRef;}
+	TRefRef				GetSchemeAssetRef()		const	{ return m_SchemeAssetRef;}
+	TSchemeUpdateType	GetUpdateType()			const	{ return m_Type; }
+		
+	inline Bool		operator==(const TRefRef SchemeRef) const	{	return (m_SchemeRef == SchemeRef);	}
+
+private:
+	TRef				m_SchemeRef;			// SchemeRef
+	TRef				m_SchemeAssetRef;		// Asset of scheme
+	TSchemeUpdateType	m_Type;					// Load or unload
+	
+	// Scheme update state modes
+	class TSchemeState_Init;		
+	class TSchemeState_Loading;		
+	class TSchemeState_UnLoading;		
+	class TSchemeState_Finished;		
+	class TSchemeState_Cancel;
+	
+};
+
+
+// Scheme update state modes
+class TLScheme::TSchemeManager::TSchemeUpdateRequest::TSchemeState_Init : public TStateMode
+{
+	virtual TRef			Update();			
+};
+
+// Handles loading a scheme
+class TLScheme::TSchemeManager::TSchemeUpdateRequest::TSchemeState_Loading : public TStateMode
+{
+	virtual TRef			Update();			
+};
+
+// Handles unloading a scheme
+class TLScheme::TSchemeManager::TSchemeUpdateRequest::TSchemeState_UnLoading : public TStateMode
+{
+	virtual TRef			Update();
+};
+
+// Holding state mode for when the scheme update is finished
+class TLScheme::TSchemeManager::TSchemeUpdateRequest::TSchemeState_Finished : public TStateMode
+{
+};
+
+// Handles cancelling a load or unload request
+class TLScheme::TSchemeManager::TSchemeUpdateRequest::TSchemeState_Cancel : public TStateMode
+{
+	virtual TRef			Update();			
 };
