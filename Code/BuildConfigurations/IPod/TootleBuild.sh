3a7b4a8b6a4c7d46188fae907752c29e959bfa37
Graham Reeves <graham@grahamreeves.com>
2009-05-15 23:22:16 +0000

    Updated the TootleBuild script to checkout a repository if it doesn't exist. Added a 2nd parameter which is your keychain (login) password. This unlocks the keychain before building so that xcode can sign the app. Build now cleans all targets then builds. (Obviously will take longer and the tootle libs will be cleaned and rebuilt several times, but it's easier.)
    Todo: email logs on failure.

--- Code/BuildConfigurations/IPod/TootleBuild.sh
+++ Code/BuildConfigurations/IPod/TootleBuild.sh
@@ -1,33 +1,70 @@
 #!/bin/sh
 # Tootle's automated build script;
-# use: ./TootleBuild.sh Seaman
+# use: ./TootleBuild.sh Seaman YourKeychainPassword
 
 # assumes you've already checked out the repositories.
 # Structure like
 # ~/TootleBuild/
 # add a symbolic link to this script in ~/TootleBuild/
 # checkout Tootle, PROJECT etc to ~/TootleBuild/Tootle and ~/TootleBuild/PROJECT
-# then call ./TootleBuild.sh PROJECT and it does the following
+# then call ./TootleBuild.sh PROJECT PASSWORD and it does the following
 
 # Update svn
+# unlocks your keychain so xcode can sign the app
 # Build 
 # Zip
 # Upload
 
+# maybe should force this to be relative to something?
+cd ~/TootleBuild/
 
 PROJECT=$1
+PASSWORD=$2
 
-echo "Updating svn..."
-svn update ~/TootleBuild/$PROJECT > ~/TootleBuild/${PROJECT}_svn.txt
+if [ "$PROJECT" == "" ]; then
+	echo "No project provided. First parameter should be the project name, which is case sensitive. "
+	exit 1
+fi
+
+# if the directory doesn't exist, assume the repository hasn't been checked out
+if [ ! -d "$PROJECT" ]; then
+	echo "$PROJECT directory doesn't exist, checking out repository..."
+	svn checkout svn://grahamgrahamreeves.getmyip.com:1983/$PROJECT $PROJECT > ${PROJECT}_svn.txt
+	
+	if [ $? != 0 ]; then
+		echo "svn checkout of $PROJECT failed. See $PROJECT_svn.txt"
+		exit 1
+	fi
+else
+	echo "Updating $PROJECT svn..."
+	svn update $PROJECT > ${PROJECT}_svn.txt
+	if [ $? != 0 ]; then
+		echo "svn update of $PROJECT failed. See $PROJECT_svn.txt"
+		exit 1
+	fi
+fi
+
+
+# make sure password was provided
+if [ "$PASSWORD" == "" ]; then
+	echo "No keychain password provided. Second parameter should be your keychain/login password to unlock the keychain so we can sign the .app"
+	exit 1
+fi
+
+# unlock keychain to sign project
+echo "Unlocking keychain..."
+security unlock-keychain -p $PASSWORD > $PROJECT_unlock.txt
+
+# error
 if [ $? != 0 ]; then
-	echo "svn update $PROJECT failed."
+	echo "Failed to unlock keychain. See $PROJECT_unlock.txt"
 	exit 1
 fi
 
 # need to change directory for xcodebuild to work :(
 echo "Building $PROJECT..."
-cd ~/TootleBuild/$PROJECT/Code/IPod/
-xcodebuild -configuration "Release AdHoc" -sdk iphoneos2.2.1 build > ~/TootleBuild/${PROJECT}_build.txt
+cd $PROJECT/Code/IPod/
+xcodebuild -configuration "Release AdHoc" -sdk iphoneos2.2.1 -alltargets clean build > ../../../${PROJECT}_build.txt
 if [ $? != 0 ]; then
 	echo "xcode build failed. read ${PROJECT}_build.txt"
 	exit 1
@@ -80,3 +117,4 @@ fi
 
 echo "Zip and upload finished. $PROJECT built!"
 exit 0
+
