066d49430a4d046b2e830254e1a48a98ebd49ec8
Graham Reeves <graham@grahamreeves.com>
2009-05-16 13:12:19 +0000

    Fixed log filename locations in buildscript. Made it update the Tootle repository before the project. Made it set permissions on the build script after updating tootle svn to make sure it can be executed next time in case the script is changed remotely.

--- Code/BuildConfigurations/IPod/TootleBuild.sh
+++ Code/BuildConfigurations/IPod/TootleBuild.sh
@@ -15,43 +15,51 @@
 # Zip
 # Upload
 
-# maybe should force this to be relative to something?
-cd ~/TootleBuild/
-
 PROJECT=$1
 PASSWORD=$2
+
+# maybe should force this to be relative to something?
+cd ~/TootleBuild/
+LOG_DIR=`pwd`
+LOG_FILE="${LOG_DIR}/Log_$PROJECT"
+
 
 if [ "$PROJECT" == "" ]; then
 	echo "No project provided. First parameter should be the project name, which is case sensitive. "
 	exit 1
 fi
 
-# if the directory doesn't exist, assume the repository hasn't been checked out
-if [ ! -d "$PROJECT" ]; then
-	echo "$PROJECT directory doesn't exist, checking out repository..."
-	svn checkout svn://grahamgrahamreeves.getmyip.com:1983/$PROJECT $PROJECT > ${PROJECT}_svn.txt
-	
-	if [ $? != 0 ]; then
-		echo "svn checkout of $PROJECT failed. See $PROJECT_svn.txt"
-		exit 1
-	fi
-else
-	echo "Updating $PROJECT svn..."
-	svn update $PROJECT > ${PROJECT}_svn.txt
-	if [ $? != 0 ]; then
-		echo "svn update of $PROJECT failed. See $PROJECT_svn.txt"
-		exit 1
-	fi
-fi
-
 
 # update the tootle repository, if this fails assume it hasn't been checked out
 echo "Updating Tootle svn repository..."
-svn update Tootle > Tootle_svn.txt
+svn update Tootle > "${LOG_DIR}/Log_Tootle_svn.txt"
 if [ $? != 0 ]; then
 	echo "svn update of Tootle failed, the Tootle Repository must have already been checked out (especially for the latest version of this script). See Tootle_svn.txt"
 	exit 1
 fi
+
+# in case the build script has changed, restore execution permissions
+chmod 777 "Tootle/Code/BuildConfigurations/IPod/TootleBuild.sh"
+
+
+# if the directory doesn't exist, assume the repository hasn't been checked out
+if [ ! -d "$PROJECT" ]; then
+	echo "$PROJECT directory doesn't exist, checking out repository..."
+	svn checkout svn://grahamgrahamreeves.getmyip.com:1983/$PROJECT $PROJECT > ${LOG_FILE}_svn.txt
+	
+	if [ $? != 0 ]; then
+		echo "svn checkout of $PROJECT failed. See ${LOG_FILE}_svn.txt"
+		exit 1
+	fi
+else
+	echo "Updating $PROJECT svn..."
+	svn update $PROJECT > ${LOG_FILE}_svn.txt
+	if [ $? != 0 ]; then
+		echo "svn update of $PROJECT failed. See ${LOG_FILE}_svn.txt"
+		exit 1
+	fi
+fi
+
 
 
 # make sure password was provided
@@ -61,21 +69,21 @@ if [ "$PASSWORD" == "" ]; then
 fi
 
 # unlock keychain to sign project
-echo "Unlocking keychain..."
-security unlock-keychain -p $PASSWORD > $PROJECT_unlock.txt
+echo "Unlocking keychain with password..."
+security unlock-keychain -p $PASSWORD > ${LOG_FILE}_unlock.txt
 
 # error
 if [ $? != 0 ]; then
-	echo "Failed to unlock keychain. See $PROJECT_unlock.txt"
+	echo "Failed to unlock keychain. See ${LOG_FILE}_unlock.txt"
 	exit 1
 fi
 
 # need to change directory for xcodebuild to work :(
 echo "Building $PROJECT..."
 cd $PROJECT/Code/IPod/
-xcodebuild -configuration "Release AdHoc" -sdk iphoneos2.2.1 -alltargets clean build > ../../../${PROJECT}_build.txt
+xcodebuild -configuration "Release AdHoc" -sdk iphoneos2.2.1 -alltargets clean build > ${LOG_FILE}_build.txt
 if [ $? != 0 ]; then
-	echo "xcode build failed. read ${PROJECT}_build.txt"
+	echo "xcode build failed. read ${LOG_FILE}_build.txt"
 	exit 1
 fi
 
@@ -106,21 +114,21 @@ FTP_DIR="public_html/tootlegames/beta/"
 
 echo "Zipping up ${APP_FILENAME}..."
 echo "Zip filename: ${ZIP_FILENAME}"
-zip "${ZIP_FILENAME}" -r "${APP_FILENAME}" > ${PROJECT}_zip.txt
+zip "${ZIP_FILENAME}" -r "${APP_FILENAME}" > ${LOG_FILE}_zip.txt
 
 # abort if we failed to zip
 if [ $? != 0 ]; then
-	echo "Failed to zip ${ZIP_FILENAME}; see ${PROJECT}_zip.txt"
+	echo "Failed to zip ${ZIP_FILENAME}; see ${LOG_FILE}_zip.txt"
 	exit 1
 fi
 
 # upload zip with curl
 echo "uploading ${ZIP_FILENAME} to ${FTP_HOST}/${FTP_DIR}..."
-curl -T "${ZIP_FILENAME}" -u ${FTP_USER}:${FTP_PASS} ftp://${FTP_HOST}/${FTP_DIR} > "${PROJECT}_ftp.txt"
+curl -T "${ZIP_FILENAME}" -u ${FTP_USER}:${FTP_PASS} ftp://${FTP_HOST}/${FTP_DIR} > "${LOG_FILE}_ftp.txt"
 
 # failed
 if [ $? != 0 ]; then
-	echo "Failed to upload ${ZIP_FILENAME}; see ${PROJECT}_ftp.txt"
+	echo "Failed to upload ${ZIP_FILENAME}; see ${LOG_FILE}_ftp.txt"
 	exit 1
 fi
 
