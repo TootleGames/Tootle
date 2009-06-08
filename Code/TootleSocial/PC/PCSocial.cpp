ee73f94a2982249676d8b9d4c3b2696745b5b59f
Duane Bradbury <dibely@hotmail.com>
2009-06-08 08:18:08 +0000

    Added skeleton PC project for the TootleSocial library

---
+++ Code/TootleSocial/PC/PCSocial.cpp
@@ -0,0 +1,19 @@
+/*
+ *  PCSocial.cpp
+ *  TootleSocial
+ *
+ *  Created by Duane Bradbury on 04/06/2009.
+ *  Copyright 2009 Tootle. All rights reserved.
+ *
+ */
+
+#include "PCSocial.h"
+
+namespace TLSocial
+{
+	namespace Platform
+	{
+		void BeginSession()		{}
+		void EndSession()		{}
+	}
+}
