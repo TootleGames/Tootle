949543af336daaf8ea410e180af49de965e2127f
Graham Reeves <graham@grahamreeves.com>
2009-06-11 19:16:25 +0000

    Added initial thumbstick widget. (Maybe final :)

---
+++ Code/TootleGame/TWidgetThumbStick.cpp
@@ -0,0 +1,75 @@
+#include "TWidgetThumbStick.h"
+#include <TootleMaths/TShapeSphere.h>
+#include <TootleRender/TRenderNode.h>
+
+
+
+TLGui::TWidgetThumbStick::TWidgetThumbStick(TRefRef RenderTargetRef,TRefRef RenderNodeRef,TRefRef UserRef,TRefRef ActionOutDown,TRefRef ActionOutUp) : 
+	TLInput::TInputInterface	( RenderTargetRef, RenderNodeRef, UserRef, ActionOutDown, ActionOutUp ),
+	m_DeadZone					( TLMaths_NearZero )
+{
+}
+
+
+
+//-------------------------------------------------
+//	process a click and detect clicks on/off our render node. return SyncWait if we didnt process it and want to process again
+//-------------------------------------------------
+SyncBool TLGui::TWidgetThumbStick::ProcessClick(TClick& Click,TLRender::TScreen& Screen,TLRender::TRenderTarget& RenderTarget,TLRender::TRenderNode& RenderNode)
+{
+	//	mouse up, dont need to do anything
+	if ( Click.GetActionValue() == 0.f )
+		return SyncFalse;
+
+	//	see if ray intersects our object, and creates a valid ray
+	SyncBool Intersection = IsIntersecting(Screen, RenderTarget, RenderNode, Click );
+	if ( Intersection != SyncTrue )
+		return Intersection;
+
+	//	get the 2d circle bounds as the thumbstick
+	//	if not valid, wait till next frame (off-screen?)
+	const TLMaths::TShapeSphere2D& BoundsSphere = RenderNode.GetWorldBoundsSphere2D();
+	if ( !BoundsSphere.IsValid() )
+		return SyncWait;
+
+	//	get vector from center of the sphere to the click point
+	float2 ClickVector = Click.GetWorldRay().GetStart().xy() - BoundsSphere.GetCenter().xy();
+
+	//	normalise vector to 0..1 in the sphere
+	ClickVector /= BoundsSphere.GetSphere().GetRadius();
+
+	//	get length to work out normalised vector and if we're in the deadzone
+	float VectorLen = ClickVector.Length();
+
+	//	in the dead zone, return TRUE (processed click) but don't send out a message
+	//	this "ignores" the click
+	if ( VectorLen <= m_DeadZone )
+		return SyncTrue;
+
+	//	send out thumb stick message
+	TRef ActionOutRef = m_ActionOutDown;
+
+
+	if ( ActionOutRef.IsValid() )
+	{
+	#ifdef _DEBUG
+		TTempString Debug_String("WidgetThumbstick (");
+		m_RenderNodeRef.GetString( Debug_String );
+		Debug_String.Append(") sending thumbstick message: ");
+		ActionOutRef.GetString( Debug_String );
+		Debug_String.Appendf(": %.2f, %.2f", ClickVector.x, ClickVector.y );
+		TLDebug_Print( Debug_String );
+	#endif		
+		
+		//	make up fake input message
+		TLMessaging::TMessage Message(TRef_Static(A,c,t,i,o));
+		Message.Write( ActionOutRef );
+		Message.ExportData("RawData", ClickVector );
+
+		//	send message
+		PublishMessage( Message );
+	}
+
+	return SyncTrue;
+}
+
---
+++ Code/TootleGame/TWidgetThumbStick.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include "TInputInterface.h"
+
+namespace TLGui
+{
+	class TWidgetThumbStick;
+}
+
+
+class TLGui::TWidgetThumbStick : public TLInput::TInputInterface
+{
+public:
+	TWidgetThumbStick(TRefRef RenderTargetRef,TRefRef RenderNodeRef,TRefRef UserRef,TRefRef ActionOutDown,TRefRef ActionOutUp=TRef());
+	
+protected:
+	virtual SyncBool		ProcessClick(TClick& Click,TLRender::TScreen& Screen,TLRender::TRenderTarget& RenderTarget,TLRender::TRenderNode& RenderNode);	//	process a click and detect clicks on/off our render node. return SyncWait if we didnt process it and want to process again
+	virtual void			OnCursorMove(const int2& NewCursorPosition)			{	QueueClick( NewCursorPosition, 1.f );	}
+
+protected:
+	float					m_DeadZone;		//	if click is within this deadzone (0..1) then ignore it
+};
