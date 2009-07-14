6c43cddf3eeefaab81056c2e3e9c32ec0a0b3438
Graham Reeves <graham@grahamreeves.com>
2009-07-14 10:33:24 +0000

    Removed the TPtr one-instance check from TBinaryTree. Logic was wrong, it's okay with referenced TPtr's... in most cases. Sometimes the compiler will generate a TPtr and put it as a reference. This check is more appropriate in uses of TPtr<> parameters which are NOT references.

--- Code/TootleCore/TBinaryTree.h
+++ Code/TootleCore/TBinaryTree.h
@@ -43,9 +43,9 @@ public:
 	TBinary&					GetData()										{	return (*this);	}
 	const TBinary&				GetData() const									{	return (*this);	}
 	Bool						CopyDataTree(const TBinaryTree& Data,Bool OverwriteDataRef=TRUE);			//	recursivly copy the tree from Data into this (allocs new data and copies the data)
-	FORCEINLINE Bool			CopyDataTree(const TPtr<TBinaryTree>& pData,Bool OverwriteDataRef=TRUE)			{	pData.Debug_CheckNotOnlyPtr();	const TBinaryTree* pBinaryTree = pData.GetObject();	return pBinaryTree ? CopyDataTree( *pBinaryTree, OverwriteDataRef ) : FALSE;	}
+	FORCEINLINE Bool			CopyDataTree(const TPtr<TBinaryTree>& pData,Bool OverwriteDataRef=TRUE)			{	const TBinaryTree* pBinaryTree = pData.GetObject();	return pBinaryTree ? CopyDataTree( *pBinaryTree, OverwriteDataRef ) : FALSE;	}
 	Bool						ReferenceDataTree(const TBinaryTree& Data,Bool OverwriteDataRef=TRUE);			//	copy the tree by re-using the TPtr's to the data. The data is re-used and saves us allocating and copying data but without fear of deletion
-	FORCEINLINE Bool			ReferenceDataTree(const TPtr<TBinaryTree>& pData,Bool OverwriteDataRef=TRUE)	{	pData.Debug_CheckNotOnlyPtr();	const TBinaryTree* pBinaryTree = pData.GetObject();	return pBinaryTree ? ReferenceDataTree( *pBinaryTree, OverwriteDataRef ) : FALSE;	}
+	FORCEINLINE Bool			ReferenceDataTree(const TPtr<TBinaryTree>& pData,Bool OverwriteDataRef=TRUE)	{	const TBinaryTree* pBinaryTree = pData.GetObject();	return pBinaryTree ? ReferenceDataTree( *pBinaryTree, OverwriteDataRef ) : FALSE;	}
 	Bool						AddUnreadChildren(TBinaryTree& Data,Bool ReplaceExisting);		//	add children from Data to this when unread - if ReplaceExisting then we overwrite existing child data with that ref
 	void						SetChildrenRead(TRefRef DataRef);				//	mark any children with this ref as read
 	Bool						IsDataTreeRead() const;							//	traverse tree to see if any data has been read (read pos is valid and not -1)
