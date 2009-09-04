19685afda48d4b3d289bb6328902b14684435269
Duane Bradbury <dibely@hotmail.com>
2009-09-04 10:30:25 +0000

    Updated the mac file sys with some recent changes - not actually working yet.

--- Code/TootleFilesys/Mac/MacLocalFileSys.h
+++ Code/TootleFilesys/Mac/MacLocalFileSys.h
@@ -1,8 +1,8 @@
 /*------------------------------------------------------
- 
- Local file system... reads files out of a specific directory
- 
- -------------------------------------------------------*/
+	
+	Local file system... reads files out of a specific directory
+
+-------------------------------------------------------*/
 #pragma once
 #include "../TLFileSys.h"
 
@@ -24,22 +24,26 @@ class TLFileSys::Platform::LocalFileSys : public TLFileSys::TFileSys
 {
 public:
 	LocalFileSys(TRefRef FileSysRef,TRefRef FileSysTypeRef);
-	
+
 	virtual SyncBool		Init();								//	check directory exists
 	virtual SyncBool		LoadFileList();						//	search for all files
 	virtual SyncBool		LoadFile(TPtr<TFile>& pFile);		//	load a file
-	virtual TPtr<TFile>		CreateFile(const TString& Filename,TRefRef FileTypeRef);
+	virtual TPtr<TFile>		CreateNewFile(const TString& Filename);
 	virtual SyncBool		WriteFile(TPtr<TFile>& pFile);
 	virtual SyncBool		Shutdown();
-	
+
 	void					SetDirectory(const TString& Directory);
-	
+	void					SetIsWritable(Bool IsWritable)		{	m_IsWritable = IsWritable;	}
+
 protected:
 	Bool					IsDirectoryValid();					//	returns FALSE if m_Directory isn't a directory
-	
+	TPtr<TFile>				CreateFileInstance(Bool LostIfEmpty);						//	create a new file from find data
+	void					UpdateFileInstance(TPtr<TFile> pFile,Bool LostIfEmpty);										//	update file info by doing a win32 search for it and fetching its file details
+
 	Bool					LoadFileList(const char* pFileSearch);	//	load files with a filter, returns number of files found. -1 on error
-	
+
 protected:
-	TString					m_Directory;
+	Bool					m_IsWritable;						//	overriding readonly setting
+	TString					m_Directory;						//	path to dir
 };
 
--- Code/TootleFilesys/Mac/MacLocalFileSys.mm
+++ Code/TootleFilesys/Mac/MacLocalFileSys.mm
@@ -9,7 +9,9 @@ using namespace TLFileSys;
 
 
 Platform::LocalFileSys::LocalFileSys(TRefRef FileSysRef,TRefRef FileSysTypeRef) :
-TFileSys			( FileSysRef, FileSysTypeRef )
+	TFileSys			( FileSysRef, FileSysTypeRef ),
+	m_FileFindHandle	( INVALID_HANDLE_VALUE ),
+	m_IsWritable		( TRUE )
 {
 }
 
@@ -19,10 +21,16 @@ TFileSys			( FileSysRef, FileSysTypeRef )
 //---------------------------------------------------------------
 SyncBool Platform::LocalFileSys::Init()
 {
+	//	directory not set - use root dir
+	if ( !m_Directory.GetLength() )
+	{
+		SetDirectory("");
+	}
+
 	//	check directory exists
 	if ( !IsDirectoryValid() )
 		return SyncFalse;
-	
+
 	return SyncTrue;
 }
 
@@ -32,6 +40,13 @@ SyncBool Platform::LocalFileSys::Init()
 //---------------------------------------------------------------
 SyncBool Platform::LocalFileSys::Shutdown()
 {
+	//	close file file handle
+	if ( m_FileFindHandle != INVALID_HANDLE_VALUE )
+	{
+		FindClose( m_FileFindHandle );
+		m_FileFindHandle = INVALID_HANDLE_VALUE;
+	}
+
 	return SyncTrue;
 }
 
@@ -42,28 +57,22 @@ SyncBool Platform::LocalFileSys::LoadFileList()
 {
 	//	mark all the current files as missing, then any files we dont 
 	//	re-find when scanning the dir we remove
-	for ( u32 f=0;	f<this->GetSize();	f++ )
+	for ( u32 f=0;	f<GetFileList().GetSize();	f++ )
 	{
-		TPtr<TFile>& pFile = this->ElementAt( f );
+		TPtr<TFile>& pFile = GetFileList().ElementAt( f );
 		if ( pFile )
 			pFile->GetFlags().Set( TFile::Lost );
 	}
-	
-	//	search for asset files first to make the file ref's match the filename as a priority
-	if ( !Platform::LocalFileSys::LoadFileList("*.asset") )
-	{
-		return SyncFalse;
-	}
-	
-	//	now search for all the other file types
+
+	//	search for all the file types
 	if ( !Platform::LocalFileSys::LoadFileList("*.*") )
 	{
 		return SyncFalse;
 	}
-	
+
 	//	finalise file list (updates timestamp and flushes missing files)
 	FinaliseFileList();
-	
+
 	return SyncTrue;
 }
 
@@ -127,10 +136,10 @@ Bool Platform::LocalFileSys::LoadFileList(const char* pFileSearch)
 		//	make a wildcard search from the directory name
 		TTempString FileSearch = m_Directory;
 		FileSearch.Append(pFileSearch);
-		
+
 		//	init the search
 		m_FileFindHandle = FindFirstFile( FileSearch.GetData(), &m_FileFindData );
-		
+
 		//	failed
 		if ( m_FileFindHandle == INVALID_HANDLE_VALUE )
 		{
@@ -139,25 +148,25 @@ Bool Platform::LocalFileSys::LoadFileList(const char* pFileSearch)
 			{
 				return TRUE;
 			}
-			
+
 			//	failed for other reason
 			return FALSE;
 		}
-		
+
 		//	pull out info of first file
 		CreateFileInstance( m_FileFindData, TRUE );
 	}
-	
-	
+
+
 	//	goto next file
 	while ( FindNextFile( m_FileFindHandle, &m_FileFindData ) )
 	{
 		CreateFileInstance( m_FileFindData, TRUE );
 	}
-	
+
 	//	find next file has failed... assume out of files?
 	//if ( GetLastError() == ERROR_NO_MORE_FILES )
-	
+
 	//	close find handle
 	FindClose( m_FileFindHandle );
 	m_FileFindHandle = INVALID_HANDLE_VALUE;
@@ -177,22 +186,23 @@ SyncBool Platform::LocalFileSys::LoadFile(TPtr<TFile>& pFile)
 		TLDebug_Break("File expected");
 		return SyncFalse;
 	}
-	
+
 	//	already loaded
 	if ( pFile->IsLoaded() == SyncTrue )
 		return SyncTrue;
-	
+
 	//	get full path and filename
 	TString FullFilename = m_Directory;
 	FullFilename.Append( pFile->GetFilename() );
-	
+
 	//	open
-	FILE* pFileHandle = fopen( FullFilename.GetData(), "rb" );
-	
+	FILE* pFileHandle = NULL;
+	errno_t Result = fopen_s( &pFileHandle, FullFilename.GetData(), "rb" );
+
 	//	failed to open
-	if ( !pFileHandle )
+	if ( Result != 0 )
 	{
-		//UpdateFileInstance( pFile, NULL );
+		UpdateFileInstance( pFile, NULL );
 		pFile->SetIsLoaded( SyncFalse );
 		return SyncFalse;
 	}
@@ -200,24 +210,24 @@ SyncBool Platform::LocalFileSys::LoadFile(TPtr<TFile>& pFile)
 	{
 		pFile->GetFlags().Clear( TFile::OutOfDate );
 	}
-	
+
 	//	move cursor to end and get position
 	fseek( pFileHandle, 0, SEEK_END );
 	s32 FileSize = ftell( pFileHandle );
-	
+
 	//	file size is zero, treat as non-existant
 	if ( FileSize == 0 )
 	{
 		fclose( pFileHandle );
-		//UpdateFileInstance( pFile, NULL );
+		UpdateFileInstance( pFile, NULL );
 		pFile->SetIsLoaded( SyncFalse );
 		return SyncFalse;
 	}
-	
-	
+
+
 	//	file data
 	TBinary& Data = pFile->GetData();
-	
+
 	//	alloc data for file
 	if ( !Data.SetSize( FileSize ) )
 	{
@@ -226,17 +236,17 @@ SyncBool Platform::LocalFileSys::LoadFile(TPtr<TFile>& pFile)
 		pFile->SetIsLoaded( SyncWait );
 		return SyncWait;
 	}
-	
+
 	//	go back to the start of the file
 	fseek( pFileHandle, 0, SEEK_SET );
-	
+
 	//	read data
 	Bool FileTooBig = FALSE;
 	u32 DataRead = 0;
 	while ( DataRead < Data.GetSize() )
 	{
 		u64 ThisRead = fread( Data.GetData(DataRead), 1, Data.GetSize()-DataRead, pFileHandle );
-		
+
 		//	too much data!
 		if ( ThisRead > (u64)0xffffffff )
 		{
@@ -244,25 +254,25 @@ SyncBool Platform::LocalFileSys::LoadFile(TPtr<TFile>& pFile)
 			FileTooBig = TRUE;
 			break;
 		}
-		
+
 		if ( ThisRead <= 0 )
 		{
 			if ( TLDebug_Break("Read zero data from file") )
 				break;
 		}
-		
+
 		DataRead += (u32)ThisRead;
 	}
-	
+
 	//	close file
 	fclose( pFileHandle );
 	pFileHandle = NULL;
-	
+
 	//	make sure file is marked as okay
 	pFile->GetFlags().Clear( TFile::Lost );
 	pFile->GetFlags().Clear( TFile::TooBig );
 	pFile->GetFlags().Clear( TFile::OutOfDate );
-	
+
 	//	file was too big
 	if ( FileTooBig )
 	{
@@ -270,10 +280,10 @@ SyncBool Platform::LocalFileSys::LoadFile(TPtr<TFile>& pFile)
 		pFile->SetIsLoaded( SyncFalse );	//	failed to load
 		return SyncFalse;
 	}
-	
+
 	//	is loaded now
 	pFile->OnFileLoaded();
-	
+
 	//	reset read pos ready for first use
 	//	gr: dont do this any more, we use the read pos to determine if we've attempted to read data post-import
 	//pFile->ResetReadPos();
@@ -358,53 +368,62 @@ void Platform::LocalFileSys::SetDirectory(const TString& Directory)
 //---------------------------------------------------------
 //	create a new empty file into file system if possible - if the filesys is read-only we cannot add external files and this fails
 //---------------------------------------------------------
-TPtr<TLFileSys::TFile> Platform::LocalFileSys::CreateFile(const TString& Filename,TRefRef FileTypeRef)
+TPtr<TLFileSys::TFile> Platform::LocalFileSys::CreateNewFile(const TString& Filename)
 {
-	return NULL;
-/*	
+ return NULL;
+
+/*
+	//	not allowed to write to this file sys
+	if ( !m_IsWritable )
+		return NULL;
+
 	//	look for existing file
-	TPtr<TFile> pFile = GetFile( Filename, FALSE );
+	TPtr<TFile>& pFile = GetFile( Filename );
 	if ( pFile )
 		return pFile;
-	
+
 	//	cant create a file if directory is invalid
 	if ( !IsDirectoryValid() )
 		return NULL;
-	
+
 	//	get full path and filename
 	TTempString FullFilename = m_Directory;
 	FullFilename.Append( Filename );
-	
+
 	//	attempt to open file before creating instance
 	FILE* pFileHandle = NULL;
 	errno_t Result = fopen_s( &pFileHandle, FullFilename.GetData(), "wb" );
-	
+
 	//	failed to open
 	if ( Result != 0 )
 	{
 		return NULL;
 	}
-	
+
 	//	close file
 	fclose( pFileHandle );
 	pFileHandle = NULL;
-	
-	
+
 	//	get file data by searching for it
 	HANDLE FileFindHandle;
 	WIN32_FIND_DATA FileFindData;
 	FileFindHandle = FindFirstFile( FullFilename.GetData(), &FileFindData );
-	
+
 	//	failed to find our new file
 	if ( FileFindHandle == INVALID_HANDLE_VALUE )
 		return NULL;
-	
+
 	FindClose( FileFindHandle );
 	FileFindHandle = INVALID_HANDLE_VALUE;
-	
+
 	//	create instance
 	TPtr<TLFileSys::TFile> pNewFile = CreateFileInstance( FileFindData, FALSE );
-	
+	if ( !pNewFile )
+	{
+		TLDebug_Break("failed to find newly created file?");
+		return NULL;
+	}
+
 	//	return new instance if it worked
 	return pNewFile;
  */
@@ -423,58 +442,64 @@ SyncBool Platform::LocalFileSys::WriteFile(TPtr<TFile>& pFile)
 		TLDebug_Break("File expected");
 		return SyncFalse;
 	}
-	
+
+	//	not allowed to write to this file sys
+	if ( !m_IsWritable )
+		return SyncFalse;
+
 	TTempString FullFilename = m_Directory;
 	FullFilename.Append( pFile->GetFilename() );
-	
+
 	//	open file for writing
-	FILE* pFileHandle = fopen( FullFilename.GetData(), "wb" );
-	
+	FILE* pFileHandle = NULL;
+	errno_t Result = fopen_s( &pFileHandle, FullFilename.GetData(), "wb" );
+
 	//	failed to open
-	if ( !pFileHandle )
+	if ( Result != 0 )
 	{
-		//UpdateFileInstance( pFile, NULL );
+		TLDebug_Break("gr: update file instance null should set loaded to false anyway? follow this code");
+		UpdateFileInstance( pFile, NULL );
 		pFile->SetIsLoaded( SyncFalse );
 		return SyncFalse;
 	}
-	
+
 	//	go back to the start of the file
 	fseek( pFileHandle, 0, SEEK_SET );
-	
+
 	//	file data
 	TBinary& Data = pFile->GetData();
-	
+
 	//	write data
 	u32 DataWritten = 0;
 	while ( DataWritten < Data.GetSize() )
 	{
 		u64 ThisWritten = fwrite( Data.GetData(DataWritten), 1, Data.GetSize()-DataWritten, pFileHandle );
-		
+
 		if ( ThisWritten <= 0 )
 		{
 			if ( TLDebug_Break("Wrote zero data to file") )
 				break;
 		}
-		
+
 		//	too much data!
 		if ( ThisWritten > (u64)0xffffffff )
 		{
 			TLDebug_Break("written more data than a u32? somethings gone wrong");
 			break;
 		}
-		
+
 		DataWritten += (u32)ThisWritten;
 	}
-	
+
 	//	close file
 	fclose( pFileHandle );
 	pFileHandle = NULL;
-	
+
 	//	file can't be out of date any more!
 	pFile->GetFlags().Clear( TFile::OutOfDate );
-	
+
 	//	refresh file info
-	//UpdateFileInstance( pFile, FALSE );
-	
+	UpdateFileInstance( pFile, FALSE );
+
 	return SyncTrue;
 }
