86663aeee0837c8d6b79a18acf76a09f7d590484
Duane Bradbury <dibely@hotmail.com>
2009-10-13 09:37:21 +0000

    Fixed an issue with the TAM file conversion using a raw TAsset pointer and then passing this around as a TPtr, consequently meaning the asset gets deleted when the TPtr went out of scope.  The raw pointer use has been changed to be a TPtr fixing the problem.  This would only show up when converting the assets for the first time.

--- Code/TootleFilesys/TFileAssetMarkup.cpp
+++ Code/TootleFilesys/TFileAssetMarkup.cpp
@@ -155,7 +155,7 @@ SyncBool TLFileSys::TFileAssetMarkup::ImportMesh_ImportTag(TPtr<TLAsset::TMesh>&
 	}
 
 	//	get the asset we're importing from
-	TLAsset::TMesh* pImportMesh = TLAsset::GetAsset<TLAsset::TMesh>( ImportMeshRef );
+	TPtr<TLAsset::TMesh> pImportMesh = TLAsset::GetAssetPtr<TLAsset::TMesh>( ImportMeshRef );
 	if ( !pImportMesh )
 	{
 		TLDebug_Print("TAM mesh import: Unknown asset");
