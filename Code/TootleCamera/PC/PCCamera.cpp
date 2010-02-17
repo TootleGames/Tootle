90ae33e3f88fe69f319748954bc495e9e281e874
Duane Bradbury <dibely@hotmail.com>
2010-02-17 16:01:57 +0000

    Added a PC version of the TootleCamera library along with skeleton PCCamera files.

---
+++ Code/TootleCamera/PC/PCCamera.cpp
@@ -0,0 +1,34 @@
+/*
+ *  PCCamera.mm
+ *  TootleCamera
+ *
+ *  Created by Duane Bradbury on 28/01/2010.
+ *  Copyright 2010 Tootle. All rights reserved.
+ *
+ */
+
+#include "PCCamera.h"
+
+using namespace TLCamera;
+
+SyncBool Platform::Initialise()
+{
+	return SyncTrue;
+}
+
+SyncBool Platform::Shutdown()
+{
+	return SyncTrue;
+}
+
+
+
+Bool Platform::ConnectToCamera()
+{
+	return FALSE;
+}
+
+Bool Platform::DisconnectFromCamera()
+{
+	return FALSE;
+}
